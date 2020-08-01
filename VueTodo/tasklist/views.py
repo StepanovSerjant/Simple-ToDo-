fb15e871a0c0796afda7b064b2bfcc149304455d
Aleksey Stepanov <stpnvlks@gmail.com>
2020-08-01 16:48:57 +0300

    Update views.py with get and post methods to work with json

--- VueTodo/tasklist/views.py
+++ VueTodo/tasklist/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.shortcuts import redirect
+
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+from .models import Task
+from .forms import TaskForm
 
 
 class TaskView(View):
 
     def get(self, request):
-        return render(request, 'tasklist/list.html', context={'name': 'Stepanov'})
+        tasks = list(Task.objects.values())
+        if request.is_ajax():
+            return JsonResponse({'tasks': tasks}, status=200)
+        return render(request, 'tasklist/list.html')
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            new_task = form.save()
+            return JsonResponse({'task': model_to_dict(form)}, status=200)
+
+        return redirect('task_list_url')
+
